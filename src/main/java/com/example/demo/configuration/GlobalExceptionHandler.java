508828787b2c710596991379a5e2afc3b9bc6282
Yadunandan <yaduahuja24@gmail.com>
2025-07-01 17:48:23 +0530

    Fixed Imports

--- src/main/java/com/example/demo/configuration/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/configuration/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.example.demo.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
